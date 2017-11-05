fb7089c0a1265dae00ed0fb245f6a0d6768d50f6
John Papa <john@Johns-MBP.lan>
2017-11-05 00:16:58 -0400

    main

--- client/main.js
+++ client/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// const HeroDetail = {
-//   template: '#herodetail'
-// };
-
 new Vue({
   el: '#app',
   template: '<App/>',
   // render: h => h(App),
   components: {
     App
-    // heroDetail: HeroDetail
   }
 })
