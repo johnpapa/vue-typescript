d42164e785caf818ea14e0b49b13593795636824
John Papa <john@Johns-MBP.lan>
2017-11-25 15:35:10 -0500

    merging

---
+++ client/components/VillainList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Villains go here
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({ })
+export default class VillainList extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+</style>
